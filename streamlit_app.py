80f3d3f89d904095383ef4a6f24229a618617ffe
Onyekachukwu Okonji <onyekaokonji@gmail.com>
2023-01-19 09:01:53 +0100

    fin

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import os, requests, torch, math, cv2
 import numpy as np
 import PIL
 import sys
-import subprocess
+# import subprocess
 
 from yolov6.utils.events import LOGGER, load_yaml
 from yolov6.layers.common import DetectBackend
@@ -10,7 +10,6 @@ from yolov6.data.data_augment import letterbox
 from yolov6.utils.nms import non_max_suppression
 from yolov6.core.inferer import Inferer
 from tools.infer import run
-
 
 import streamlit as st
 
@@ -23,7 +22,7 @@ def videoInput():
     if uploaded_video != None:
 
         vidpath = os.path.join('data/videos', uploaded_video.name)
-        outputpath = os.path.join('runs/inference/exp', os.path.basename(vidpath))
+        outputpath = os.path.join('runs', 'inference')
         cfg_model_path = os.path.join('assets/weights', 'yolov6n.pt')
 
         with open(vidpath, mode='wb') as f:
@@ -32,14 +31,16 @@ def videoInput():
         st_video = open(vidpath, 'rb')
         video_bytes = st_video.read()
         st.video(video_bytes)
-        st.write("Uploaded Video")
+        st.caption("Uploaded Video")
+        st.write("Please wait while the objects are detected")
         run(weights=cfg_model_path, source=vidpath, yaml=os.path.join('data', 'coco.yaml'), img_size=640, half=False,
-            conf_thres=0.4, iou_thres=0.45, max_det=1000, save_txt=False, not_save_img=False, save_dir=None, view_img=True,
+            conf_thres=0.4, iou_thres=0.45, max_det=1000, save_txt=False, not_save_img=False, save_dir=None, view_img=False,
             classes=None, agnostic_nms=False, project=outputpath, hide_labels=False, hide_conf=False)
-        st_video2 = open(outputpath, 'rb')
+        final_output_path = os.path.join(outputpath, 'exp/') + uploaded_video.name
+        st_video2 = open(final_output_path, 'rb')
         video_bytes2 = st_video2.read()
         st.video(video_bytes2)
-        st.write("Model Prediction")
+        st.caption("Model Prediction")
 
 def main():
     # -- Sidebar
